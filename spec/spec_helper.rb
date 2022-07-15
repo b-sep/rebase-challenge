d0c616e73ff938ac24bc3cbe275854c111cd56ec
Júnior <b.sep@live.com>
2022-07-15 10:40:23 -0300

    Usa banco diferente se o ambiente for de teste

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,15 @@
-ENV['RACK_ENV'] = 'test'
-require 'rack/test'
-require 'rake'
-require 'debug'
+ENV["RACK_ENV"] = "test"
+require 'rubygems'
+require 'bundler'
+Bundler.require(:default, :test)
 
 RSpec.configure do |config|
   config.include Rack::Test::Methods
 
   config.before(:suite) do
     Rake.load_rakefile './Rakefile'
-    DatabaseCleaner.clean_with :truncation
     DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
   end
 
   config.around(:each) do |clean|
