fa4738c85f7d6a6d12e6e1f2cea71c1b0febc73d
Júnior <b.sep@live.com>
2022-07-16 18:20:30 -0300

    Refatoração do método de importação

--- import.rb
+++ import.rb
@@ -5,15 +5,20 @@ require 'sinatra/activerecord'
 require_relative 'models/exam'
 
 class Import
-  def self.parse_file(csv)
-    csv_parse = CSV.parse(csv, col_sep: ';', headers: true, header_converters: :symbol)
-  end
+  MAPPING = { cpf: :cpf, patient_name: :nome_paciente, patient_email: :email_paciente, patient_birth_date: :data_nascimento_paciente, patient_address: :endereorua_paciente, patient_city: :cidade_paciente, patient_state: :estado_patiente, doctor_registration: :crm_mdico, doctor_registration_state: :crm_mdico_estado, doctor_name: :nome_mdico, doctor_email: :email_mdico, token_exam_result: :token_resultado_exame, exam_date: :data_exame, exam_type: :tipo_exame, exam_limit: :limites_tipo_exame, exam_result: :resultado_tipo_exame }
 
-  def self.from_csv(csv)
-    csv_parse = parse_file(csv)
-    csv_parse.each do |row|
-      Exam.create!(cpf: row[:cpf], patient_name: row[:nome_paciente], patient_email: row[:email_paciente],
-                    patient_birth_date: row[:data_nascimento_paciente], patient_address: row[:endereorua_paciente], patient_city: row[:cidade_paciente], patient_state: row[:estado_patiente], doctor_registration: row[:crm_mdico], doctor_registration_state: row[:crm_mdico_estado], doctor_name: row[:nome_mdico], doctor_email: row[:email_mdico], token_exam_result: row[:token_resultado_exame], exam_date: row[:data_exame], exam_type: row[:tipo_exame], exam_limit: row[:limites_tipo_exame], exam_result: row[:resultado_tipo_exame])
+  class << self
+    def parse_file(file)
+      csv_parse = CSV.parse(file, col_sep: ';', headers: true, header_converters: :symbol)
+    end
+
+    def from_csv(file)
+      csv_parse = parse_file(file)
+      csv_parse.each do |row|
+        attrs = MAPPING.transform_values { |value| row[value] }
+        Exam.create!(attrs)
+      end
     end
   end
+
 end
